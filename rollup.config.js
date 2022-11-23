cdd55147f4ee984eaf8b3511ed12b92c08387621
lmx <limx@deepcool.com>
2022-11-23 10:01:49 +0800

    fix: 解决rollup的TypeScript插件打包异常问题

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 const typescript = require("@rollup/plugin-typescript");
 const terser = require("@rollup/plugin-terser");
 const commonjs = require("@rollup/plugin-commonjs");
+const node = require("@rollup/plugin-node-resolve");
 
 module.exports = {
     input: "./src/electron/index.ts",
@@ -8,10 +9,13 @@ module.exports = {
         file: "src/cache/main.js",
         format: "cjs"
     },
-    external: ["electron", "path"],
+    external: ["electron", "path"], // 不想打包的第三方包
     plugins: [
-        typescript({ include: "electron" }),
+        typescript({
+             tsconfig: "./tsconfig.electron.json"
+         }),
+        commonjs(),
+        node.nodeResolve(),
         terser(),
-        commonjs()
     ]
 }
